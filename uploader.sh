66d70e62c74b56e6bd0ee5b92b82325daab5c549
irritable-chen <963711691@qq.com>
2023-06-17 16:16:24 +0800

    no

--- uploader.sh
+++ uploader.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 # 各类配置信息
-base_url="https://github.com/MichaelChan1st/typora"
+base_url="https://github.com/MichaelChan1st/typora/blob/main"
+
 
 for i in "$@"
 do 
-	git add "$i"
-	git commit -m "$i"
+	mv $i /Users/radicalism/Desktop/Notes/upload-pic/typora/
+done
+for i in "$@"
+do
+	suffix=$(basename $i)
+	git add "$suffix"
+	git commit -m "$suffix"
 	git push
 done
-
 # 输出结果
 echo "Upload Success:"
 for file in "$@"
 do	
-	IFS='/' read -r -a array <<< "$file"
-	id="${#array[@]}"
-	echo "$base_url"/"${array[$id-1]}"
+	suffix1=$(basename $file)
+	echo "$base_url"/"$suffix1"
+
 done
